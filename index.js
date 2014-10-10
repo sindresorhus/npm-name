eedfdfed508a47c23b70c87b4fdb76b9096cee12
Sindre Sorhus <sindresorhus@gmail.com>
2014-10-10 17:22:54 +0200

    bump `registry-url`

--- index.js
+++ index.js
@@ -3,19 +3,17 @@ var got = require('got');
 var registryUrl = require('registry-url');
 
 module.exports = function (name, cb) {
-	registryUrl(function (err, url) {
-		got(url + encodeURIComponent(name), {method: 'HEAD'}, function (err) {
-			if (err === 404) {
-				cb(null, true);
-				return;
-			}
+	got(registryUrl + encodeURIComponent(name), {method: 'HEAD'}, function (err) {
+		if (err === 404) {
+			cb(null, true);
+			return;
+		}
 
-			if (err) {
-				cb(err);
-				return;
-			}
+		if (err) {
+			cb(err);
+			return;
+		}
 
-			cb(null, false);
-		});
+		cb(null, false);
 	});
 };
