efd130af687da59a7c3391ae8a1b46fc8b64e24e
Kevin Martensson <kevinmartensson@gmail.com>
2015-10-16 19:07:43 +0200

    Simplify CLI help creation

--- cli.js
+++ cli.js
@@ -4,20 +4,18 @@ var logSymbols = require('log-symbols');
 var meow = require('meow');
 var npmName = require('./');
 
-var cli = meow({
-	help: [
-		'Usage',
-		'  $ npm-name <name>',
-		'',
-		'Examples',
-		'  $ npm-name chalk',
-		'  ' + logSymbols.error + ' Unavailable',
-		'  $ npm-name unicorn-cake',
-		'  ' + logSymbols.success + ' Available',
-		'',
-		'Exits with code 0 when the name is available or 2 when taken'
-	]
-});
+var cli = meow([
+	'Usage',
+	'  $ npm-name <name>',
+	'',
+	'Examples',
+	'  $ npm-name chalk',
+	'  ' + logSymbols.error + ' Unavailable',
+	'  $ npm-name unicorn-cake',
+	'  ' + logSymbols.success + ' Available',
+	'',
+	'Exits with code 0 when the name is available or 2 when taken'
+]);
 
 if (cli.input.length === 0) {
 	console.error('Package name required');
