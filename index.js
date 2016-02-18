68625ee79a99294be184eaeb41fc459e53a64156
alexandresantos72 <alexandre.santos@spms.min-saude.pt>
2016-02-18 11:41:37 +0000

    New proxy configuration

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 'use strict';
 var got = require('got');
 var registryUrl = require('registry-url')();
-var Promise = require('pinkie-promise');
+var path = require('path');
+var NPMRC = process.env.NPMRC || path.join(process.env.HOME || process.env.USERPROFILE, '.npmrc');
+var properties = require('properties');
+var ProxyAgent = require('proxy-agent');
+
+function config() {
+	return new Promise(function (resolve) {
+		properties.parse(NPMRC, {
+			path: true
+		}, function (error, obj) {
+			if (error) {
+				return resolve(null);
+			}
+			return resolve(obj[Object.keys(obj).find((value) => value === 'https-proxy')]);
+		});
+	});
+}
 
 module.exports = function (name) {
 	if (!(typeof name === 'string' && name.length !== 0)) {
 		return Promise.reject(new Error('Package name required'));
 	}
-
-	return got.head(registryUrl + name.toLowerCase()).then(function () {
-		return false;
-	}).catch(function (err) {
-		if (err.statusCode === 404) {
-			return true;
-		}
-
-		throw err;
-	});
+	return config()
+	.then((url) => got.head(registryUrl + name.toLowerCase(), url ? {agent: new ProxyAgent(url)} : {}))
+	.then(() => false)
+	.catch((err) => err.statusCode === 404 ? true : err);
 };
