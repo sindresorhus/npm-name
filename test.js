2c71d151d1d0d89bdaffa1354235edfca9126eaa
Sindre Sorhus <sindresorhus@gmail.com>
2016-03-16 19:05:27 +0700

    improve tests
    closes #6

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import fn from './';
 
 test('returns true when package name is available', async t => {
-	t.true(await fn('asdasfgrgafadsgaf'));
+	t.true(await fn('asdasfgrgafadsgaf' + Math.random().toString().slice(2)));
 });
 
 test('returns false when package name is taken', async t => {
