476a23f9ca695a38b1fed6114eaa884125f5029c
Jason Maurer <j@maur.co>
2016-04-06 18:46:15 -0600

    weirdness with tabs

--- index.js
+++ index.js
@@ -4,29 +4,29 @@ var registryUrl = require('registry-url')();
 var Promise = require('pinkie-promise');
 
 module.exports = function (name) {
-	if (!name instanceof String || !name instanceof Array || !name.length) {
-		return Promise.reject(new Error('Package name required'));
-	}
+  if (!name instanceof String || !name instanceof Array || !name.length) {
+    return Promise.reject(new Error('Package name required'));
+  }
 
-	if (name instanceof Array) {
-		var queue = [];
-		for (var i = 0, len = name.length; i < len; i++) {
-			queue.push(makeRequest(name[i]));
-		}
-		return Promise.all(queue);
-	}
+  if (name instanceof Array) {
+    var queue = [];
+    for (var i = 0, len = name.length; i < len; i++) {
+      queue.push(makeRequest(name[i]));
+    }
+    return Promise.all(queue);
+  }
 
-	return makeRequest(name);
+  return makeRequest(name);
 };
 
 function makeRequest(name) {
-	return got.head(registryUrl + name.toLowerCase()).then(function () {
-		return false;
-	}).catch(function (err) {
-		if (err.statusCode === 404) {
-			return true;
-		}
+  return got.head(registryUrl + name.toLowerCase()).then(function () {
+    return false;
+  }).catch(function (err) {
+    if (err.statusCode === 404) {
+      return true;
+    }
 
-		throw err;
-	});
+    throw err;
+  });
 }
--- test.js
+++ test.js
@@ -2,31 +2,31 @@ import test from 'ava';
 import fn from './';
 
 test('returns true when package name is available', async t => {
-	t.true(await fn('asdasfgrgafadsgaf' + Math.random().toString().slice(2)));
+  t.true(await fn('asdasfgrgafadsgaf' + Math.random().toString().slice(2)));
 });
 
 test('returns false when package name is taken', async t => {
-	t.false(await fn('chalk'));
+  t.false(await fn('chalk'));
 });
 
 test('returns true when package names are available', async t => {
-	var name1 = 'asdasfgrgafadsgaf' + Math.random().toString().slice(2);
-	var name2 = 'ihvoiwehbioboiwwq' + Math.random().toString().slice(2);
-	var res = await fn([name1, name2]);
-	t.false(res.indexOf(false) > -1);
+  var name1 = 'asdasfgrgafadsgaf' + Math.random().toString().slice(2);
+  var name2 = 'ihvoiwehbioboiwwq' + Math.random().toString().slice(2);
+  var res = await fn([name1, name2]);
+  t.false(res.indexOf(false) > -1);
 });
 
 test('returns false when package names are taken', async t => {
-	var name1 = 'chalk';
-	var name2 = 'npm-name';
-	var res = await fn([name1, name2]);
-	t.false(res.indexOf(true) > -1);
+  var name1 = 'chalk';
+  var name2 = 'npm-name';
+  var res = await fn([name1, name2]);
+  t.false(res.indexOf(true) > -1);
 });
 
 test('returns true and false', async t => {
-	var name1 = 'chalk';
-	var name2 = 'ihvoiwehbioboiwwq' + Math.random().toString().slice(2);
-	var res = await fn([name1, name2]);
-	t.true(res.indexOf(true) > -1);
-	t.true(res.indexOf(false) > -1);
+  var name1 = 'chalk';
+  var name2 = 'ihvoiwehbioboiwwq' + Math.random().toString().slice(2);
+  var res = await fn([name1, name2]);
+  t.true(res.indexOf(true) > -1);
+  t.true(res.indexOf(false) > -1);
 });
