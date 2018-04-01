f315a2bbb6acb1fbc355d1465411538ed801046e
Sindre Sorhus <sindresorhus@gmail.com>
2018-04-01 17:51:47 +0700

    Update `got`

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ function request(name) {
 	if (isScoped(name)) {
 		name = name.replace(/\//g, '%2f');
 	}
-	return got.head(registryUrl + name.toLowerCase(), {
-		timeout: 10000
-	})
+
+	return got.head(registryUrl + name.toLowerCase(), {timeout: 10000})
 		.then(() => false)
 		.catch(err => {
 			if (err.statusCode === 404) {
