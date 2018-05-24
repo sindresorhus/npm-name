52122941cfe833dd1effbd017250ba0ccf6e16b1
astur <astur@yandex.ru>
2018-05-24 17:14:39 +0300

    Add test for invalid names

--- test.js
+++ test.js
@@ -26,3 +26,10 @@ test('returns false when scoped package name is taken', async t => {
 	t.false(await m(`@sindresorhus/is`));
 });
 
+test('throws when package name is invalid', async t => {
+	const e = await t.throws(m('_ABC'));
+	t.is(e.message, 'Invalid package name: _ABC');
+	t.is(e.warnings[0], 'name can no longer contain capital letters');
+	t.is(e.errors[0], 'name cannot start with an underscore');
+});
+
