77a752e0f17f79e8d2d6f4eb44769801bffc5638
Sindre Sorhus <sindresorhus@gmail.com>
2018-08-20 12:07:31 +0700

    Update `got` and require Node.js 8

--- index.js
+++ index.js
@@ -7,52 +7,53 @@ const validate = require('validate-npm-package-name');
 
 class InvalidNameError extends Error {}
 
-function request(name) {
+const request = async name => {
 	const isValid = validate(name);
 	if (!isValid.validForNewPackages) {
 		const notices = [...isValid.warnings || [], ...isValid.errors || []].map(v => `- ${v}`);
 		notices.unshift(`Invalid package name: ${name}`);
-		const err = new InvalidNameError(notices.join('\n'));
-		err.warnings = isValid.warnings;
-		err.errors = isValid.errors;
-		return Promise.reject(err);
+		const error = new InvalidNameError(notices.join('\n'));
+		error.warnings = isValid.warnings;
+		error.errors = isValid.errors;
+		throw error;
 	}
 
-	const isScopedRepo = isScoped(name);
-	if (isScopedRepo) {
+	const isScopedPackage = isScoped(name);
+	if (isScopedPackage) {
 		name = name.replace(/\//g, '%2f');
 	}
 
-	return got.head(registryUrl + name.toLowerCase().replace(/[-_.]/g, ''), {timeout: 10000})
-		.then(() => false)
-		.catch(err => {
-			if (err.statusCode === 404) {
-				return true;
-			}
+	try {
+		await got.head(registryUrl + name.toLowerCase().replace(/[-_.]/g, ''), {timeout: 10000});
+		return false;
+	} catch (error) {
+		if (error.statusCode === 404) {
+			return true;
+		}
 
-			if (isScopedRepo && err.statusCode === 401) {
-				return true;
-			}
+		if (isScopedPackage && error.statusCode === 401) {
+			return true;
+		}
 
-			throw err;
-		});
-}
+		throw error;
+	}
+};
 
-module.exports = name => {
-	if (!(typeof name === 'string' && name.length !== 0)) {
-		return Promise.reject(new Error('Package name required'));
+module.exports = async name => {
+	if (!(typeof name === 'string' && name.length > 0)) {
+		throw new Error('Package name required');
 	}
 
 	return request(name);
 };
 
-module.exports.many = names => {
+module.exports.many = async names => {
 	if (!Array.isArray(names)) {
-		return Promise.reject(new TypeError(`Expected an array, got ${typeof names}`));
+		throw new TypeError(`Expected an array, got ${typeof names}`);
 	}
 
-	return Promise.all(names.map(request))
-		.then(result => new Map(zip(names, result)));
+	const result = await Promise.all(names.map(request));
+	return new Map(zip(names, result));
 };
 
 module.exports.InvalidNameError = InvalidNameError;
