7b1a6b88228da6cf2d1db4c405c2daed374b949f
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-03-31 10:38:31 +0000

    Refactor TypeScript definition to CommonJS compatible export (#34)

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,62 @@
+declare class InvalidNameErrorClass extends Error {}
+
 declare const npmName: {
 	/**
-	 * Check whether a package name is available (not registered) on npm.
-	 *
-	 * @param name - Name to check.
-	 * @returns Whether the given name is available.
-	 */
+	Check whether a package name is available (not registered) on npm.
+
+	@param name - Name to check.
+	@returns Whether the given name is available.
+
+	@example
+	```
+	import npmName = require('npm-name');
+
+	(async () => {
+		console.log(await npmName('chalk'));
+		//=> false
+
+		try {
+			await npmName('_ABC');
+		} catch (error) {
+			console.log(error.message);
+			// Invalid package name: _ABC
+			// - name cannot start with an underscore
+			// - name can no longer contain capital letters
+		}
+	})();
+	```
+	*/
 	(name: string): Promise<boolean>;
 
 	/**
-	 * Check whether multiple package names are available (not registered) on npm.
-	 *
-	 * @param names - Multiple names to check.
-	 * @returns A `Map` of name and status.
-	 */
-	many<NameType extends string>(names: NameType[]): Promise<Map<NameType, boolean>>;
+	Check whether multiple package names are available (not registered) on npm.
+
+	@param names - Multiple names to check.
+	@returns A `Map` of name and status.
+
+	@example
+	```
+	import npmName = require('npm-name');
+
+	(async () => {
+		const result = await npmName.many(['chalk', '@sindresorhus/is', 'abc123']);
+		console.log(result.get('chalk'));
+		//=> false
+		console.log(result.get('@sindresorhus/is'));
+		//=> false
+		console.log(result.get('abc123'));
+		//=> true
+	})();
+	```
+	*/
+	many<NameType extends string>(
+		names: NameType[]
+	): Promise<Map<NameType, boolean>>;
+
+	InvalidNameError: typeof InvalidNameErrorClass;
+
+	// TODO: remove this in the next major version
+	default: typeof npmName;
 };
 
-export default npmName;
-
-export class InvalidNameError extends Error {}
+export = npmName;
